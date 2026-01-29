b2589bd9f321b3f434397fd8f356878c4e747065
Aijing LI <renoxevans@gmaiil.com>
2026-01-29 14:13:11 +0100

    fix: correct contact json filename

--- php-framework/app/src/Controllers/ContactController.php
+++ php-framework/app/src/Controllers/ContactController.php
@@ -39,13 +39,11 @@ class ContactController extends AbstractController{
         ];
 
         $contact_dir = __DIR__.'/../../var/contacts';
-        $emailSafe = preg_replace('/[^A-Za-z0-9._@-]/', '_', $contact['email']);
-        $file_name = $contact['dateOfCreation'].'_'.$emailSafe.'.json'; // what if mail have carac not allowed ?
-        file_put_contents($file_name, json_encode($contact, JSON_PRETTY_PRINT));
+        $emailSafe = preg_replace('/[^A-Za-z0-9_@-]/', '_', $contact['email']); //may put a wrong email address ?
+        $timestamp = date('Y-m-d_H-i-s', $contact['dateOfCreation']);
+        $file_name = $timestamp.'_'.$emailSafe.'.json';
+        file_put_contents($contact_dir.'/'.$file_name, json_encode($contact, JSON_PRETTY_PRINT));
 
-
-
-
-        return new Response('Saved', 201, []);
+        return new Response("", 201, ['Content-Type' => 'application/json']);
     }
 }
