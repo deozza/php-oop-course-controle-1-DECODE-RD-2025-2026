7c2c0e68a3dadb73b8dc09454c77137f738746c7
Mathis <vidueiramathis@gmail.com>
2026-01-29 19:54:10 +0100

    Exercice 1 : Post Contact (route, controller, entity)

--- php-framework/app/src/Lib/Http/Request.php
+++ php-framework/app/src/Lib/Http/Request.php
@@ -2,26 +2,57 @@
 
 namespace App\Lib\Http;
 
-class Request {
+class Request
+{
     private string $uri;
     private string $method;
     private array $headers;
+    private ?string $body = null;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->uri = $_SERVER['REQUEST_URI'];
         $this->method = $_SERVER['REQUEST_METHOD'];
         $this->headers = getallheaders();
+
+        if ($this->method === 'POST') {
+            $this->body = file_get_contents('php://input');
+        }
     }
 
-    public function getUri(): string {
+    public function getUri(): string
+    {
         return $this->uri;
     }
 
-    public function getMethod(): string {
+
+    public function getMethod(): string
+    {
         return $this->method;
     }
 
-    public function getHeaders(): array {
+    public function getHeaders(): array
+    {
         return $this->headers;
+    }
+
+    public function getBody(): ?string
+    {
+
+        return $this->body;
+    }
+
+    public static function isJson(?string $string): bool
+    {
+        if (is_null($string)) {
+            return false;
+        }
+        json_decode($string);
+        return (json_last_error() == JSON_ERROR_NONE);
+    }
+
+    public function checkMethod(string $method): bool
+    {
+        return $this->method === $method;
     }
 }
